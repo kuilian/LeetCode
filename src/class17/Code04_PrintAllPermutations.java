b9c72b59a6df6a094614c829f7931295994c25e8
kuilian <994743350@qq.com>
2022-09-20 22:59:25 +0800

    暴力递归到动态规划一

--- src/class17/Code04_PrintAllPermutations.java
+++ src/class17/Code04_PrintAllPermutations.java
@@ -7,32 +7,34 @@ public class Code04_PrintAllPermutations {
 
 	public static List<String> permutation1(String s) {
 		List<String> ans = new ArrayList<>();
-		if (s == null || s.length() == 0) {
+		if(s==null || s.length()==0){
 			return ans;
 		}
 		char[] str = s.toCharArray();
 		ArrayList<Character> rest = new ArrayList<Character>();
-		for (char cha : str) {
-			rest.add(cha);
+		for(char cur:str){
+			rest.add(cur);
 		}
 		String path = "";
-		f(rest, path, ans);
+		f(rest,path, ans);
 		return ans;
 	}
-
-	public static void f(ArrayList<Character> rest, String path, List<String> ans) {
-		if (rest.isEmpty()) {
-			ans.add(path);
-		} else {
-			int N = rest.size();
-			for (int i = 0; i < N; i++) {
-				char cur = rest.get(i);
-				rest.remove(i);
-				f(rest, path + cur, ans);
-				rest.add(i, cur);
+	public static void f(List<Character> rest, String path, List<String> ans)
+	{
+//		base case
+			if (rest.isEmpty()){
+				ans.add(path);
+			}else{
+				for (int i = 0; i < rest.size(); i++) {
+					Character cur = rest.get(i);
+					rest.remove(i);
+					f(rest,path+cur,ans);
+					rest.add(i,cur);
+				}
 			}
-		}
+
 	}
+
 
 	public static List<String> permutation2(String s) {
 		List<String> ans = new ArrayList<>();
